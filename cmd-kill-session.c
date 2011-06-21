4d7f9f169674015b5c52880d273d35c6920f659a
Anmol Khirbat <anmol@khirbat.net>
2011-06-21 10:31:13 -0700

    Add shorter kill-session alias

--- cmd-kill-session.c
+++ cmd-kill-session.c
@@ -30,7 +30,7 @@
 int	cmd_kill_session_exec(struct cmd *, struct cmd_ctx *);
 
 const struct cmd_entry cmd_kill_session_entry = {
-	"kill-session", NULL,
+	"kill-session", "kills",
 	"t:", 0, 0,
 	CMD_TARGET_SESSION_USAGE,
 	0,
